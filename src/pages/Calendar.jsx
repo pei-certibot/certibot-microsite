bd6a2d8aa836c321e7510406a33d828ef24a2bd9
Rodrigo Santos <rodrigo.mvs@ua.pt>
2025-10-27 17:08:59 +0000

    new version of gantt graph

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -17,16 +17,35 @@ const data = [
     { type: "number", label: "Percent Complete" },
     { type: "string", label: "Dependencies" },
   ],
-  ["MS1", "Milestone 1 Preparation", "Milestone", new Date(2025, 8, 29), new Date(2025, 9, 28), null, 100, null], 
-  ["MS2", "Milestone 2 Preparation", "Milestone", new Date(2025, 9, 28), new Date(2025, 10, 18), null, 100, null],
-  ["MS3", "Milestone 3 Preparation", "Milestone", new Date(2025, 10, 18), new Date(2025, 11, 16), null, 100, null], 
-  ["MS1-Personas", "Personas", "Paulo", new Date(2025, 9, 6), new Date(2025, 9, 28), null, 100, null], 
-  ["MS1-UserStories", "User Stories", "Manel", new Date(2025, 8, 29), new Date(2025, 9, 13), null, 100, null],
-  ["MS1-MicroSite", "Microsite", "Paulo", new Date(2025, 9, 6), new Date(2025, 9, 28), null, 100, null], 
-  ["MS1-Calendar", "Calendar", "Paulo", new Date(2025, 9, 6), new Date(2025, 9, 28), null, 100, null], 
-  ["MS1-Context", "Context / State of the Art", "Paulo", new Date(2025, 9, 6), new Date(2025, 9, 28), null, 100, null], 
-  ["MS1-Arch", "Architecture", "João", new Date(2025, 9, 6), new Date(2025, 9, 28), null, 100, null], 
-  ["MS1-Presentation", "Presentation", "Maria", new Date(2025, 9, 6), new Date(2025, 9, 28), null, 100, null], 
+["MS1", "M1-Inception", "Milestone", new Date(2025, 8, 29), new Date(2025, 9, 28), null, 100, null],
+["MS2", "M2-Elaboration", "Milestone", new Date(2025, 9, 28), new Date(2025, 10, 18), null, 100, null],
+["MS3", "M3-Construction", "Milestone", new Date(2025, 10, 19), new Date(2025, 11, 16), null, 100, null],
+["MS4", "M4-Transition", "Milestone", new Date(2026, 0, 1), new Date(2026, 5, 30), null, 100, null],
+
+["MS1-Personas", "Personas", null, new Date(2025, 8, 29), new Date(2025, 9, 14), null, 100, null],
+["MS1-UserStories", "User Stories", null, new Date(2025, 8, 29), new Date(2025, 9, 14), null, 100, null],
+["MS1-StateOfTheArtAnalysis", "State of the Art Analysis", null, new Date(2025, 8, 29), new Date(2025, 9, 14), null, 100, null],
+["MS1-Microsite", "Microsite", null, new Date(2025, 8, 29), new Date(2025, 9, 14), null, 100, null],
+
+["MS1-FunctionalRequirements", "Functional Requirements", null, new Date(2025, 9, 14), new Date(2025, 9, 28), null, 100, null],
+["MS1-InitialMockupSketch", "Initial Mockup Sketch", "Everyone", new Date(2025, 9, 14), new Date(2025, 9, 28), null, 100, null],
+["MS1-CalendarPlanning", "Calendar Planning", null, new Date(2025, 9, 14), new Date(2025, 9, 28), null, 100, null],
+["MS1-Presentation", "Presentation", null, new Date(2025, 9, 14), new Date(2025, 9, 28), null, 100, null],
+
+["MS2-RefineHighLevelArchitecture", "Refine High-Level Architecture", null, new Date(2025, 9, 28), new Date(2025, 10, 8), null, 100, null],
+["MS2-RefineMockupSketch", "Refine Mockup Sketch", "Everyone", new Date(2025, 9, 28), new Date(2025, 10, 8), null, 100, null],
+["MS2-DefineKnowledgeDatabaseStructure", "Define Knowledge Database Structure", null, new Date(2025, 10, 8), new Date(2025, 10, 18), null, 100, null],
+["MS2-LeverageLLMComplianceGuidance", "Leverage LLM to build compliance guidance from norms", null, new Date(2025, 10, 8), new Date(2025, 10, 18), null, 100, null],
+
+["MS3-RefineKnowledgeExtractionUsingLLM", "Refine Knowledge Extraction Using LLM", null, new Date(2025, 10, 19), new Date(2025, 11, 5), null, 100, null],
+["MS3-DefineChatBotAPIEndpoints", "Define ChatBot API endpoints", null, new Date(2025, 10, 19), new Date(2025, 11, 5), null, 100, null],
+["MS3-DefineKnowledgeExtractionAPIEndpoints", "Define API endpoints for the Knowledge extraction service", null, new Date(2025, 10, 19), new Date(2025, 11, 5), null, 100, null],
+
+["MS3-DevelopChatbotBackendAndUI", "Develop Chatbot Backend and UI", null, new Date(2025, 11, 5), new Date(2025, 11, 16), null, 100, null],
+["MS3-DefineUsersRelationalDatabase", "Define Users Relational Database", null, new Date(2025, 11, 5), new Date(2025, 11, 16), null, 100, null],
+["MS3-ChatbotHistoryFormatStorage", "Chatbot History Format / Storage", null, new Date(2025, 11, 5), new Date(2025, 11, 16), null, 100, null],
+
+["MS4-TBD", "TBD", null, new Date(2026, 0, 1), new Date(2026, 5, 30), null, 100, null],
 ];
 
 const options = {
